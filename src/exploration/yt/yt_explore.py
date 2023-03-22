e01293f1166d547aa61d501d6b023db8d31168ed
Lucy Farnik <lucyfarnik@gmail.com>
2023-03-22 20:23:24 +0000

    YT data fetching of any number of comments

--- src/exploration/yt/yt_explore.py
+++ src/exploration/yt/yt_explore.py
@@ -1,47 +1,100 @@
 import os
 import sys
+import math
 import requests
 import transformers
-import numpy as np
+import pandas as pd
 
 sys.path.append(os.path.abspath('.'))
 from src.constants import google_key as key
 
-classifier = transformers.pipeline("sentiment-analysis")
+num_comments = int(1e5)
+query = 'AI'
+date_range_start = '2018-01-01T00:00:00Z'
+date_range_end = '2020-01-01T00:00:00Z'
 
-vid_res = requests.get('https://www.googleapis.com/youtube/v3/search', {
-    'key': key,
-    'part': 'snippet',
-    'q': 'AI',
-    'publishedBefore': '2018-01-01T00:00:00Z',
-}).json()
-videos = [{'id': x['id']['videoId'], 'title': x['snippet']['title']}
-           for x in vid_res['items']]
+# init the classifier
+classifier = transformers.pipeline('sentiment-analysis',
+                                   'distilbert-base-uncased-finetuned-sst-2-english')
 
-for vid in videos:
-  com_res = requests.get('https://www.googleapis.com/youtube/v3/commentThreads', {
+comments = []
+next_page_token = None
+for page_i in range(math.ceil(num_comments / (50*50))):
+  # fetch some videos - construct params
+  vid_req_params = {
       'key': key,
       'part': 'snippet',
-      'videoId': vid['id'],
-      'maxResults': 100,
-  }).json()
-  if 'error' in com_res: continue
-  comments = [{'id': x['id'],
-              'text': x['snippet']['topLevelComment']['snippet']['textDisplay'],
-              'channel': x['snippet']['topLevelComment']['snippet']['authorChannelId']['value'],
-              }
-              for x in com_res['items']]
-
-  for com in comments:
-    cls_res = classifier(com['text'][:512])[0]
-    cls_results = [[x['text'], ] for x in comments]
-    print(f"\"{com['text']}\" is {cls_res['label']} ({100*cls_res['score']:.1f}%)\n")
-
-  chan_res = requests.get('https://www.googleapis.com/youtube/v3/channels', {
-      'key': key,
-      'part': 'snippet',
-      'id': ','.join([c['channel'] for c in comments[:50]]),
+      'q': query,
       'maxResults': 50,
-  }).json()
-  has_country = np.array(['country' in c['snippet'] for c in chan_res['items']])
-  print(f"{100*has_country.sum()/has_country.size:.2f}% have country codes")
+  }
+  if next_page_token is not None: # pagination
+    vid_req_params['pageToken'] = next_page_token
+
+  # date range
+  if date_range_start is not None:
+    vid_req_params['publishedAfter'] = date_range_start
+  if date_range_end is not None:
+    vid_req_params['publishedBefore'] = date_range_end
+  
+  vid_res = requests.get('https://www.googleapis.com/youtube/v3/search',
+                         vid_req_params).json()
+  if 'error' in vid_res: # TODO error handling
+    print(vid_res['error'])
+    continue
+  next_page_token = vid_res['nextPageToken'] # store page token
+  videos = [{'id': x['id']['videoId'],
+            'title': x['snippet']['title'],
+            'date': x['snippet']['publishedAt'],
+            }
+            for x in vid_res['items']]
+
+  # for each video, get the comments
+  for vid_i, vid in enumerate(videos):
+    print(f'{page_i=} {vid_i=}')
+    # fetch comments
+    com_res = requests.get('https://www.googleapis.com/youtube/v3/commentThreads', {
+        'key': key,
+        'part': 'snippet',
+        'videoId': vid['id'],
+        'maxResults': 50,
+    }).json()
+    if 'error' in com_res: # TODO error handling
+      print(com_res['error'])
+      continue
+    vid_comments = [{'id': x['id'],
+                'text': x['snippet']['topLevelComment']['snippet']['textDisplay'],
+                'channel': x['snippet']['topLevelComment']['snippet']['authorChannelId']['value'],
+                'date': x['snippet']['topLevelComment']['snippet']['publishedAt'],
+                'vid_id': vid['id'],
+                'vid_title': vid['title'],
+                'vid_date': vid['date'],
+                }
+                for x in com_res['items']]
+
+    # classify the text
+    cls_res = classifier([com['text'][:512] for com in vid_comments])
+    for com_i, cls in enumerate(cls_res):
+      vid_comments[com_i]['sentiment'] = cls
+
+    # get the channel of the user who posted it to find the country
+    chan_res = requests.get('https://www.googleapis.com/youtube/v3/channels', {
+        'key': key,
+        'part': 'snippet',
+        'id': ','.join([c['channel'] for c in vid_comments[:50]]),
+        'maxResults': 50,
+    }).json()
+    if 'error' in chan_res: # TODO error handling
+      print(chan_res['error'])
+      continue
+    for chan in chan_res['items']: # match the channel with the comment (the order isn't synced)
+      country = chan['snippet']['country'] if 'country' in chan['snippet'] else None
+      for com in vid_comments:
+        if com['channel'] == chan['id']: com['country'] = country
+    
+    # append to global results array
+    for com in vid_comments: comments.append(com)
+
+# save the fetched data
+df = pd.DataFrame(comments)
+print(df.shape, df.head())
+df.to_csv('data/yt_data.csv')
